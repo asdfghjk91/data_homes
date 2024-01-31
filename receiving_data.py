cb21797de706ea779098f7b2a30247a7994b46fc
lostlyray <energypromc11@gmail.com>
2024-01-31 10:21:30 +0300

    second commit

--- receiving_data.py
+++ receiving_data.py
@@ -1,7 +1,12 @@
 """This module has components that are used for testing tuya's device control and Pulsar massage queue."""
 import logging
-import os 
+import os
+import time
 import re
+import pandas as pd
+import openpyxl
+import datetime
+from threading import Thread
 from tuya_connector import (
     TuyaOpenAPI,
     TuyaOpenPulsar,
@@ -22,17 +27,43 @@ openapi = TuyaOpenAPI(API_ENDPOINT, ACCESS_ID, ACCESS_KEY)
 openapi.connect()
 
 # Call any API from Tuya
-DEVICE_ID ="bfa931ed1d78c0fad7mkll"
+DEVICE_ID_BRICK = "bfa931ed1d78c0fad7mkll"
+DEVICE_ID_Аeratedconcrete = "bfa6826e5e45e21a82u08p"
 
-response = openapi.get("/v1.0/iot-03/devices/{}/status".format(DEVICE_ID))
+# response = openapi.get("/v1.0/iot-03/devices/{}/status".format(DEVICE_ID_BRICK))
 
-if os.path.exists("energy.txt"):
-    os.remove("D:\IoT\energy.txt")
 
-with open("energy.txt","w+") as my_file:
+def data_energy(response):
     response = str(response)
     exchange = response[21:70]
     pattern = r"'value': (\d+)"
     match = re.search(pattern, exchange)
     value = int(match.group(1))
-    my_file.write(str(value))
+    return value
+
+
+def change_data(data):
+    data = data[:-2]+"."+data[len(data)-2:]
+
+    return data
+
+
+brick_old_data = int(data_energy(openapi.get(
+    "/v1.0/iot-03/devices/{}/status".format(DEVICE_ID_BRICK))))
+aercon_old_data = int(data_energy(openapi.get(
+    "/v1.0/iot-03/devices/{}/status".format(DEVICE_ID_Аeratedconcrete))))
+df = pd.DataFrame([], columns=['Time', 'Аerated concrete', 'Brick'])
+
+while True:
+    time.sleep(300)
+    brick_new_data = int(data_energy(openapi.get(
+        "/v1.0/iot-03/devices/{}/status".format(DEVICE_ID_BRICK))))
+    aercon_new_data = int(data_energy(openapi.get(
+        "/v1.0/iot-03/devices/{}/status".format(DEVICE_ID_Аeratedconcrete))))
+    df.loc[len(df)] = [datetime.datetime.now(), float(change_data(str(brick_new_data))) - float(change_data(
+        str(brick_old_data))), float(change_data(str(aercon_new_data))) - float(change_data(str(aercon_old_data)))]
+    brick_old_data = int(data_energy(openapi.get(
+        "/v1.0/iot-03/devices/{}/status".format(DEVICE_ID_BRICK))))
+    aercon_old_data = int(data_energy(openapi.get(
+        "/v1.0/iot-03/devices/{}/status".format(DEVICE_ID_Аeratedconcrete))))
+    df.to_excel('./energy.xlsx', index=False)
